ac79ae6b5f5b32a3055e210ec9f5dc2abe662cbd
Jonni Åkesson <devbyjonni@gmail.com>
2025-03-27 18:48:01 +0100

    chore: initial commit with solution structure

---
+++ TaskMasterApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TaskMasterConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Dotnet Console App is Running! 🚀");
+        }
+    }
+}
+
